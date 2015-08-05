bc040a1ddd50f5963e7dc412e7f4562f1e3270ef
BruceWeng <weng8916@gmail.com>
2015-08-05 15:34:57 +0800

    Create new message

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.page(params[:page]).per(10)
+  end
+
+  def show
+    @messages = Message.find(params[:id])
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.save!
+    redirect_to root_path
+  end
+
+  protected
+
+  def message_params
+    params.require(:message).permit(:title, :content, :category_name)
+  end
+end
